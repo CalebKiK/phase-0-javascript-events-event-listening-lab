e3d983c0810aa86fa6a0232eb2c13215d29d6ed8
CalebKiK <karimicaleb@gmail.com>
2024-07-11 19:13:33 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const input = document.getElementById("button");
+
 function addingEventListener() {
+    function buttonClick() {
+        alert ("I was clicked!");
+    }
+    input.addEventListener("click", buttonClick);
 }
+
+
